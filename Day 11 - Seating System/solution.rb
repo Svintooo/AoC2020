ef6d097b260d1855a5fec7c1da324acaf31cbd58
Hugo Ankarloo <hugo@ilait.se>
2020-12-13 00:35:18 +0100

    Day 11: Initial commit

---
+++ Day 11 - Seating System/solution.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+## Load input
+(puts "USAGE: #{__FILE__} input_file"; exit) unless ARGV.any?
+file_path = ARGV[0]
+(puts "File not found: #{file_path}"; exit) unless File.file?(file_path)
+input = IO.read(file_path)
+
+
+
+## Parse input
+asdf = input.split(/\n+/)
