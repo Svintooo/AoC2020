d05e8a6807fcf637c2cf302d6b7de95c9ac8119b
Hugo Ankarloo <hugo@ilait.se>
2020-12-13 01:22:00 +0100

    Problem 1: first try for a solution

--- Day 11 - Seating System/solution.rb
+++ Day 11 - Seating System/solution.rb
@@ -9,4 +9,50 @@ input = IO.read(file_path)
 
 
 ## Parse input
-asdf = input.split(/\n+/)
+#seats = input.split(/\n+/).collect{|str| str.chars.collect(&:to_sym) }
+seats = input.split(/\n+/).collect{|str| str.chars }
+#seats = input.split(/\n+/)
+
+def seats.adjacent(i, j)
+  adjacent_seats_status = []
+
+  [
+    [-1, -1], [-1,  0], [-1, +1],
+    [ 0, -1],           [ 0, +1],
+    [+1, -1], [+1,  0], [+1, +1],
+  ].each do |i_delta, j_delta|
+    adj_i = i + i_delta
+    adj_j = j + j_delta
+    next if adj_i < 0 || adj_i >= self.count
+    next if adj_j < 0 || adj_j >= self[i].count
+    adjacent_seats_status << self[adj_i][adj_j]
+  end
+
+  return adjacent_seats_status
+end
+
+
+
+## Answer 1
+loop do
+  seatings_has_changed = false
+  
+  (0).upto(seats.count-1).each do |i|
+    (0).upto(seats[i].count-1).each do |j|
+      seat_before_change = seats[i][j]
+
+      case seats[i][j]
+        when "L" then seats[i][j] = "#" if seats.adjacent(i,j).count("#") == 0
+        when "#" then seats[i][j] = "L" if seats.adjacent(i,j).count("#") > 4
+      end
+
+      seatings_has_changed |= (seats[i][j] != seat_before_change)
+    end
+  end
+  
+  break if not seatings_has_changed
+end
+
+
+
+## Answer 2
