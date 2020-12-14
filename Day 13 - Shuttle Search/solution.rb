62b68394866a21d40f96c08c98b8bfcbc4e7e0fa
Svintooo <github.com@svintoo.com>
2020-12-14 03:40:28 +0100

    [Day13](P2): Increment by highest Bus ID
    Incrementing with a higher number, less loops neeeded. Code
    execution time goes down.
    
    But I must confess: The code is ugly as hell. I suspect that I will
    not be able to understand it if I look back on it in the future.

--- Day 13 - Shuttle Search/solution.rb
+++ Day 13 - Shuttle Search/solution.rb
@@ -33,21 +33,30 @@ bus_ids = line2.split(",").collect{|bus_id| bus_id =~ /^[0-9]+$/ ? bus_id.to_i :
 #
 work_array = Array.new(bus_ids.count, 0)
 indexes = bus_ids.each_with_index.to_a.select{|bus_id,_| bus_id != 0 }.collect{|_,index| index }
-increment = bus_ids.first
-timestamp = 100000000000000 - (100000000000000 % increment) + increment
-indexes.shift
+max_bus_id = bus_ids.max
+max_bus_id_index = indexes.delete( bus_ids.index(max_bus_id) )
+timestamp = 100000000000000 - (100000000000000 % max_bus_id) + max_bus_id
+work_array.each_index do |i|
+  next if bus_ids[i] == 0
+  work_array[i] = timestamp % bus_ids[i]
+end
 
 loop do
-  timestamp += increment
+  timestamp += max_bus_id
   success = true
 
   indexes.each do |i|
-    work_array[i] = (work_array[i] + increment) % bus_ids[i]
-    wait_time = (bus_ids[i] - work_array[i])  # Minutes you have to wait for Bus ID `bus_ids[i]` if time is `timestamp`
+    work_array[i] = (work_array[i] + max_bus_id) % bus_ids[i]
+    work_value = (work_array[i] - max_bus_id_index) % bus_ids[i]
+    wait_time = (bus_ids[i] - work_value) % bus_ids[i]  # Minutes you have to wait for Bus ID `bus_ids[i]` if time is `timestamp`
     success &= wait_time == i
   end
 
-  break if success
+  if success
+    timestamp -= work_array[0]
+    break
+  end
+  #puts timestamp if timestamp.to_s =~ /^#{max_bus_id}0+$/
 end
 
 # Print
